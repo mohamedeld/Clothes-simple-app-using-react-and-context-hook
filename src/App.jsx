807314a169a6727142a8f03bd2e2e86f496bb4f1
mohamedeld <mohamed.azoz20010@gmail.com>
2023-10-28 11:07:05 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import Header from "./components/Header.jsx";
+import Shop from "./components/Shop.jsx";
+import {
+  CartContextProvider
+} from "./store/shopping-cart-context.js";
+
+function App() {
+  return (
+    <>
+      <CartContextProvider>
+        <Header />
+        <Shop />
+      </CartContextProvider>
+    </>
+  );
+}
+
+export default App;
